9c79ce79e7b52a7f5ab3b1368db35e1c30eb3214
Jared <38332939+jaredaritter@users.noreply.github.com>
2019-09-24 15:03:00 -0400

    basic landing page and campground route

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send('Welcome Home');
+    res.render('landing');
 });
+
+app.get('/campgrounds', (req, res) => {
+    var campgrounds = [
+        {name: 'Salmon Creek', image: 'https://s3.amazonaws.com/images.gearjunkie.com/uploads/2016/08/blm-camping.jpg'},
+        {name: 'Granite Hill', image: 'https://s3.amazonaws.com/images.gearjunkie.com/uploads/2016/08/blm-camping.jpg'},
+        {name: 'Mountain Goats Rest', image: 'https://s3.amazonaws.com/images.gearjunkie.com/uploads/2016/08/blm-camping.jpg'}
+    ];
+    res.render('campgrounds', {campgrounds: campgrounds});
+})
 
 app.get('*', (req, res) => {
     res.send("Are you lost?")
