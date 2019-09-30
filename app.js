eb916216d87fd4d4abff4cd8a1de9fd8dd4caca4
Jared <38332939+jaredaritter@users.noreply.github.com>
2019-09-30 12:05:37 -0400

    added mongoose and switched to mongoDB for persistent storage from array

--- app.js
+++ app.js
@@ -1,29 +1,49 @@
-const express = require('express');
-const request = require('request');
-const bodyParser = require('body-parser');
-const app = express();
+const express = require('express'),
+      request = require('request'),
+      bodyParser = require('body-parser'),
+      mongoose = require('mongoose'),
+      app = express();
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost/yelp_camp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-var campgrounds = [
-    {name: 'Salmon Creek', image: 'https://www.tripsavvy.com/thmb/B99SUpBFjesrl8TFUJBSrGAY0SA=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-508066351-5a12280d9e942700376da496.jpg'},
-    {name: 'Granite Hill', image: 'https://s3.amazonaws.com/images.gearjunkie.com/uploads/2016/08/blm-camping.jpg'},
-    {name: 'Mountain Goats Rest', image: 'https://img.sunset02.com/sites/default/files/styles/4_3_horizontal_inbody_900x506/public/image/2016/07/main/litter-free-campground-sun-0816.jpg'},
-    {name: 'Salmon Creek', image: 'https://www.tripsavvy.com/thmb/B99SUpBFjesrl8TFUJBSrGAY0SA=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-508066351-5a12280d9e942700376da496.jpg'},
-    {name: 'Granite Hill', image: 'https://s3.amazonaws.com/images.gearjunkie.com/uploads/2016/08/blm-camping.jpg'},
-    {name: 'Mountain Goats Rest', image: 'https://img.sunset02.com/sites/default/files/styles/4_3_horizontal_inbody_900x506/public/image/2016/07/main/litter-free-campground-sun-0816.jpg'},
-    {name: 'Salmon Creek', image: 'https://www.tripsavvy.com/thmb/B99SUpBFjesrl8TFUJBSrGAY0SA=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-508066351-5a12280d9e942700376da496.jpg'},
-    {name: 'Granite Hill', image: 'https://s3.amazonaws.com/images.gearjunkie.com/uploads/2016/08/blm-camping.jpg'},
-    {name: 'Mountain Goats Rest', image: 'https://img.sunset02.com/sites/default/files/styles/4_3_horizontal_inbody_900x506/public/image/2016/07/main/litter-free-campground-sun-0816.jpg'}
-];
+// Schema Setup
+var campgroundSchema = new mongoose.Schema({
+    name: String, 
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create({
+//     name: "Salmon Creek",
+//     image: "https://www.tripsavvy.com/thmb/B99SUpBFjesrl8TFUJBSrGAY0SA=/960x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-508066351-5a12280d9e942700376da496.jpg"
+// }, (err, campground) => {
+//     if (err) {
+//         console.log(err);
+//     } else { 
+//         console.log("New campground: ");
+//         console.log(campground);
+//     }
+// });
 
 app.get('/', (req, res) => {
     res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-    res.render('campgrounds', {campgrounds: campgrounds});
+    // get all campgrounds from DB
+    Campground.find({}, (err, allCampgrounds) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('campgrounds', {campgrounds: allCampgrounds});
+        }
+    })
 });
 
 app.get('/campgrounds/new', (req, res) => {
@@ -35,9 +55,15 @@ app.post('/campgrounds', (req, res) => {
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    // redirect back to campgrounds page
-    res.redirect('/campgrounds');
+    // create a new campground and save to DB
+    Campground.create(newCampground, (err, newlyCreated) => {
+        if (err) {
+            console.log(err);
+        } else {
+            // redirect back to campgrounds page
+            res.redirect('/campgrounds');
+        }
+    });
 });
 
 app.get('*', (req, res) => {
