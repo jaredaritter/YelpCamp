75b8ae22dc0a6a835b44cd1c20f3eec88f5bdd76
Jared <38332939+jaredaritter@users.noreply.github.com>
2020-01-10 15:46:42 -0500

    Included ownership check on comments for edit and destroy

--- routes/comments.js
+++ routes/comments.js
@@ -45,7 +45,7 @@ router.post('/', isLoggedIn, (req, res) => {
 });
 
 // EDIT COMMENT
-router.get('/:comment_id/edit', (req, res) => {
+router.get('/:comment_id/edit', checkCommentOwnership, (req, res) => {
     Comment.findById(req.params.comment_id, (err, foundComment) => {
         if (err) {
             res.redirect('back');
@@ -56,7 +56,7 @@ router.get('/:comment_id/edit', (req, res) => {
 })
 
 // UPDATE COMMENT
-router.put('/:comment_id', (req, res) => {
+router.put('/:comment_id', checkCommentOwnership, (req, res) => {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
         if (err) {
             res.redirect('back');
@@ -67,7 +67,7 @@ router.put('/:comment_id', (req, res) => {
 })
 
 // DESTROY COMMENT
-router.delete('/:comment_id', (req, res) => {
+router.delete('/:comment_id', checkCommentOwnership, (req, res) => {
     Comment.findByIdAndRemove(req.params.comment_id, (err) => {
         if (err) {
             res.redirect('back');
@@ -86,4 +86,24 @@ function isLoggedIn(req, res, next){
     res.redirect('/login');
 }
 
+function checkCommentOwnership(req, res, next) {
+    if (req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, (err, foundComment) => {
+            if (err) {
+                res.redirect('back');
+            } else {
+                // does user own comment?
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect('back');
+                }
+            }
+        })
+    } else {
+        res.redirect('back');
+    }
+}
+
+// MODULES
 module.exports = router;
