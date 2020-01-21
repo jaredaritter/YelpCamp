d84a88bb10bd9e506d45bbbb7ec4dab1230076c4
Jared <38332939+jaredaritter@users.noreply.github.com>
2020-01-21 18:07:44 -0500

    Dynamic pricing added

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -24,13 +24,14 @@ router.get('/new', middleware.isLoggedIn, (req, res) => {
 router.post('/', middleware.isLoggedIn, (req, res) => {
     // get data from form and add to campground array
     var name = req.body.name;
+    var price = req.body.price;
     var image = req.body.image;
     var desc = req.body.description;
     var author = {
         id: req.user._id,
         username: req.user.username
     };
-    var newCampground = {name: name, image: image, description: desc, author: author};
+    var newCampground = {name: name, price: price, image: image, description: desc, author: author};
     // create a new campground and save to DB
     Campground.create(newCampground, (err, newlyCreated) => {
         if (err) {
